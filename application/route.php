92d779d4f4d003610426243c7fa78b9e4690261b
xgwshen <xgwshen@gmail.com>
2018-05-21 17:26:00 +0800

    'ss'

--- application/route.php
+++ application/route.php
@@ -9,13 +9,20 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 
-return [
-    '__pattern__' => [
-        'name' => '\w+',
-    ],
-    '[hello]'     => [
-        ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
-        ':name' => ['index/hello', ['method' => 'post']],
-    ],
-    'redis' => ['index/redis', ['method' => 'get']],
-];
+//return [
+//    '__pattern__' => [
+//        'name' => '\w+',
+//    ],
+//    '[hello]'     => [
+//        ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
+//        ':name' => ['index/hello', ['method' => 'post']],
+//    ],
+//    'redis' => ['index/redis', ['method' => 'get']],
+////    'login' => ['index/login', ['method' => 'get']],
+//    Route::any('login','index/login',['method'=>'get']),
+//];
+
+use think\Route;
+// 注册路由到index模块的News控制器的read操作
+Route::get('login/:id','index/index/login',['id'=>'/d']);
+
